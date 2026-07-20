561c976716e55f064f6de1a83ca0bc4093f58bfa
juanmoore-creator <moorejuanf@gmail.com>
2026-07-20 18:07:44 -0300

    second commit

--- busapp.py
+++ busapp.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask, render_template, request, jsonify
+from whitenoise import WhiteNoise
 from services.cuandollega import obtener_arribos
 
 app = Flask(__name__, static_folder="static")
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="static/")
 
 
 @app.route("/")
